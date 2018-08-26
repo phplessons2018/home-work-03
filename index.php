9f4345c56670c39e76e8192d8080ac815212eae3
phplessons2018 <s.dimitrenco@webdelo.org>
2018-08-26 15:32:36 +0300

    Made first and second tasks

--- index.php
+++ index.php
@@ -1,44 +1,39 @@
 <?php
 
-//$file = file_get_contents('data.xml');
-//
-//$getData = new SimpleXMLElement($file);
-//
-////print_r($getData);
-//
-//echo '<h1>'. $getData->getName() . '</h1>';
-//echo 'PurchaseOrderNumber' . ' : ' . (string)$getData["PurchaseOrderNumber"] . '<br><br>';
-//echo 'OrderDate' . ' : ' . (string)$getData["OrderDate"] . '<br><br>';
-//
-//foreach ($getData as $key => $item) {
-//    if ($key != "Items") {
-//        echo '<h2>'. (string)$item->attributes()->Type . '</h2>' . '<br>' . $item->getName() . ' : ' . (string)$item . '<br>'  ;
-//        foreach ($item as $value) {
-//            echo $value->getName() . ' : ' . (string)$value . '<br>';
-//    }
-//        echo '<br>';
-//    } elseif ($key == "Items") {
-//        foreach ($item as $value) {
-//            foreach ($value as $v) {
-//                echo $v->getName() . ' : ' . (string)$v . '<br>';
-//
-//            }
-//            echo '<br>';
-//
-//        }
-//    }
-//}
+$file = file_get_contents('data.xml');
+
+$getData = new SimpleXMLElement($file);
+
+//print_r($getData);
+
+echo '<h1>'. $getData->getName() . '</h1>';
+echo 'PurchaseOrderNumber' . ' : ' . (string)$getData["PurchaseOrderNumber"] . '<br><br>';
+echo 'OrderDate' . ' : ' . (string)$getData["OrderDate"] . '<br><br>';
+
+foreach ($getData as $key => $item) {
+    if ($key != "Items") {
+        echo '<h2>'. (string)$item->attributes()->Type . '</h2>' . '<br>' . $item->getName() . ' : ' . (string)$item . '<br>'  ;
+        foreach ($item as $value) {
+            echo $value->getName() . ' : ' . (string)$value . '<br>';
+    }
+        echo '<br>';
+    } elseif ($key == "Items") {
+        foreach ($item as $value) {
+            foreach ($value as $v) {
+                echo $v->getName() . ' : ' . (string)$v . '<br>';
+
+            }
+            echo '<br>';
+
+        }
+    }
+}
 
 
 
 //task2
 
-//1. Создайте массив, в котором имеется как минимум 1 уровень вложенности.
-//Преобразуйте его в JSON. Сохраните как output.json
-//2. Откройте файл output.json. Случайным образом, используя функцию rand(), решите
-//изменять данные или нет. Сохраните как output2.json
-//3. Откройте оба файла. Найдите разницу и выведите информацию об отличающихся
-//элементах
+
 
 function createJSON ()
 {
@@ -48,13 +43,75 @@ function createJSON ()
           2,
           3,
           4,
+      ],
+      [
+          11,
+          22,
+          33,
+          44,
       ]
     ];
 
 
     $createData = json_encode($data);
+    file_put_contents('my.json', $createData);
 
 }
 
+createJSON ();
 
 
+
+
+function createNewJSON ()
+{
+    $data = file_get_contents('my.json');
+    $decoded = json_decode($data);
+
+
+
+    $rand = rand(0, 1);
+
+    if ($rand == 1) {
+        array_push($decoded, 11);
+        $createData = json_encode($decoded);
+    } else {
+        $createData = json_encode($decoded);
+    }
+
+    print_r($createData);
+
+    file_put_contents('my2.json', $createData);
+}
+
+createNewJSON ();
+
+
+
+
+function resultJSON () {
+    $data = file_get_contents('my.json');
+    $decoded = json_decode($data);
+
+    $data2 = file_get_contents('my2.json');
+    $decoded2 = json_decode($data2);
+
+    $result = array_diff_assoc($decoded, $decoded2);
+
+//    echo 'Результат разницы двух массивов - ' . $result;
+
+}
+
+resultJSON ();
+
+
+
+//task3
+
+//function random_array($n, $min = 0, $max = 100) {
+//    return array_map(function() use ($min, $max) { return mt_rand($min, $max); }, range(1, $n));
+//
+//
+//}
+//
+//var_dump(random_array(50));
